8012a74a9c9c13b41555e95f18d5a299a72a78a7
Mitchell Barton <mitchell@ringseven.com>
2016-06-30 00:53:35 -0600

    adding new responsive stuff

--- js/script.js
+++ js/script.js
@@ -9,17 +9,29 @@ var color = ['rgba(0, 0, 255, 1)',
 
 var index = 0;
 var currentColor;
-var images = ['../images/book-photo-1.jpg',
-              '../images/book-photo-2.jpg',
-              '../images/book-photo-3.jpg',
-              '../images/book-photo-4.jpg',
-              '../images/book-photo-5.jpg',
-              '../images/book-photo-cover.jpg',
+var images = ['../images/b-1.jpg',
+              '../images/b-2.jpg',
+              '../images/b-3.jpg',
+              '../images/b-4.jpg',
+              '../images/b-5.jpg',
+              '../images/b-6.jpg',
+              '../images/b-7.jpg',
+              '../images/b-8.jpg',
+              '../images/b-9.jpg',
+              '../images/b-10.jpg',
+              '../images/b-11.jpg',
+              '../images/fig-4.jpg',
+              '../images/fig-5.jpg',
+              '../images/net-presence-1.0.jpg',
+              '../images/screenshot-1-new.jpg',
+              '../images/green-rock-3.jpg',
               '../images/amazon-1.jpg',
               '../images/amazon-4.jpg',
               '../images/amazon-5.jpg',
               '../images/t-rex-1.jpg',
               '../images/lightbulb-1.jpg',
+              '../images/suggestion-partner-1.jpg',
+              '../images/suggestion-partner-2.jpg',
               '../images/j-3-2.jpg',
               '../images/j-3.jpg',
               '../images/j-7.jpg',
@@ -27,27 +39,21 @@ var images = ['../images/book-photo-1.jpg',
               '../images/jordan-ad4-print.jpg',
               '../images/jordan-ad5-print.jpg',
               '../images/jordan-ad6-print.jpg',
+              '../images/book-photo-cover.jpg',
+              '../images/book-photo-1.jpg',
+              '../images/book-photo-2.jpg',
+              '../images/book-photo-3.jpg',
+              '../images/book-photo-4.jpg',
+              '../images/book-photo-5.jpg',
               '../images/pseudo-1.jpg',
               '../images/pseudo-2.jpg',
               '../images/pseudo-5.jpg',
               '../images/pseudo-7.jpg',
-              '../images/fig-4.jpg',
-              '../images/fig-5.jpg',
-              '../images/personal-website.jpg',
-              '../images/green-rock-3.jpg',
-              '../images/suggestion-partner-1.jpg',
-              '../images/suggestion-partner-2.jpg',
               '../images/rising-falling-photo.jpg',
-              '../images/rising-falling-phtot2.jpg',
-              '../images/screenshot-1-new.jpg',
-              '../images/b-1.jpg',
-              '../images/b-2.jpg',
-              '../images/b-3.jpg',
-              '../images/b-4.jpg',
-              '../images/b-5.jpg'];
+              '../images/rising-falling-phtot2.jpg'];
 
 function preLoad() {
-   
+
   function loadit(el) {
       el.onload = function() {
     };
@@ -99,9 +105,6 @@ function menuFadeIn() {
   opacityAn('.bk-hm', '1');
   $('.menu').css('visibility', 'visible');
   $('.bk-hm').css('visibility', 'visible');
-  // if ($(window).width() < 800) {
-  //   $('.menu').css({'position':'relative', 'height':'none'});
-  // }
 }
 
 function menuFadeDummy () {
@@ -174,6 +177,10 @@ function strobe9() {
   strobe('a.screenshots');
 }
 
+function strobe10() {
+  strobe('a.net-presence');
+}
+
 function flash(link, color, fn) {
   var flashing;
 
@@ -188,36 +195,52 @@ function flash(link, color, fn) {
     } else {
       $(this).css('color', color);
     }
-    
+
   });
 }
 
 function projectFade(project, url) {
-  $(project).css('display', 'block');
-  $(project).css('opacity', '1');
   var images = '.' + url + ' img';
   var videos = '.' + url + ' iframe';
   var container = '.view .' + url + ' .image-group';
   var titles = '.' + url + ' .piecetitle';
   var ps = '.' + url + ' p';
+  var im = $(images);
+  var vi = $(videos);
+  var pa = $(ps);
+  if ($(window).width() > 440) {
+    im.add(vi).add(pa).each(function() {
+      var rando = Math.random() * 22;
+      rando = Math.floor(rando);
+      var mrando = Math.random() * 100;
+      if (mrando > 49) {
+        $(this).css('left', rando + '%');
+      } else {
+        $(this).css('right', rando + '%');
+      }
+    });
+  }
+
+  $(project).css('display', 'block');
+  $(project).css('opacity', '1');
+
   setTimeout(function() {
     opacityAn(images, '1');
     opacityAn(videos, '1');
     opacityAn(titles, '1');
     opacityAn(ps, '1');
-    /////// NOT SURE WHY THIS IS WORKING
-    if ($(window).width > 440) {
-      $(container).css('transform', 'translateY(92px)');
-    } else {
+    if ($(window).width() > 440) {
       $(container).css('transform', 'translateY(120px)');
+    } else {
+      $(container).css('transform', 'translateY(92px)');
     }
 
     setTimeout(function() {
       opacityAn('.view .page-info', '1');
-      if ($(window).width > 440) {
-        $('.view .page-info').css('transform', 'translateY(25px)');
-      } else {
+      if ($(window).width() > 440) {
         $('.view .page-info').css('transform', 'translateY(49px)');
+      } else {
+        $('.view .page-info').css('transform', 'translateY(25px)');
       }
     }, 400);
 
@@ -236,18 +259,59 @@ function loadContent(url, fn) {
 
 function footerClick(project, fn) {
   $('.footer a').click(function(event) {
-    event.preventDefault();    
+    event.preventDefault();
     var href = this.href;
     var $this = $(this);
     if ($this.hasClass('footer-current')) {
-      
+
     } else {
       opacityAn(project, '0');
       setTimeout(function() {
         history.pushState('', 'Mitchell Barton', href);
         fn();
       }, 300);
-    } 
+    }
+  });
+}
+
+// var full = document.getElementsByTagName('fullscreen');
+
+function goFull() {
+  var fullIm;
+
+  $('.image-group img').click(function() {
+    var s = $(this).attr('src');
+    opacityAn('.menu-open', '0');
+    opacityAn('.view', '0');
+    $('body').css('overflow-y', 'hidden');
+    $('.fullscreen').addClass('zoomed');
+    setTimeout(function() {
+      $('.menu-open, .view').css('visibility', 'hidden');
+
+      fullIm = document.createElement('img');
+      fullIm.className = 'zoomed';
+      fullIm.src = s;
+      $('.fullscreen').append(fullIm);
+      $('.fullscreen').css({'visibility':'visible', 'z-index':'100'});
+      setTimeout(function() {
+        $('.fullscreen img').css({'transform':'scale(1)', '-ms-transform':'scale(1)', '-webkit-transform':'scale(1)'});
+        opacityAn('.fullscreen img', '1');
+        $('.fullscreen img').click(function() {
+          opacityAn('.fullscreen img', '0');
+          $('.fullscreen').removeClass('zoomed');
+          setTimeout(function() {
+            $('.fullscreen').css({'visibility':'hidden', 'z-index':'-10'});
+            $('.fullscreen img').remove();
+            $('.menu-open, .view').css('visibility', 'visible');
+            $('body').css('overflow-y', 'scroll');
+            setTimeout(function() {
+              opacityAn('.menu-open', '1');
+              opacityAn('.view', '1');
+            }, 300);
+          }, 300);
+        });
+      }, 300);
+    }, 300);
   });
 }
 
@@ -281,9 +345,10 @@ function gusto(p1, p2, p3) {
       setTimeout(function() {
         opacityAn('.footer', '1');
       }, 600);
+      goFull();
       footerClick(p2, p3);
       titleView();
-    }, 600);
+    }, 100);
   });
 }
 
@@ -291,7 +356,7 @@ function switchPage() {
   var currentPg = window.location.href;
   var site = 'http://mitchellbarton.com';
   if (currentPg === (site) || currentPg === (site + '/')) {
-    
+
   } else if (currentPg === (site + '/pseudorandom-landscape')) {
     gusto('pseudorandom', '.view .pseudorandom', switchPage);
   } else if (currentPg === (site + '/rising-falling')) {
@@ -302,6 +367,8 @@ function switchPage() {
     gusto('suggestions', '.view .suggestions', switchPage);
   } else if (currentPg === (site + '/black-sand-white-sand-grey-sand')) {
     gusto('black-sand-white-sand-grey-sand', '.view .black-sand-white-sand-grey-sand', switchPage);
+  } else if (currentPg === (site + '/net-presence')) {
+    gusto('net-presence', '.view .net-presence', switchPage);
   } else if (currentPg === (site + '/a-little-bit-cooler')) {
     gusto('a-little-bit-cooler', '.view .a-little-bit-cooler', switchPage);
   } else if (currentPg === (site + '/screenshots')) {
@@ -321,27 +388,44 @@ function switchPage() {
         }, 600);
         footerClick('.view .cv', switchPage);
       }, 800);
-      
+
     });
   } else {
     $('body').empty();
     $('body').load('pages/wrong-page.html');
-    
+
   }
 }
 
-$(window).load(function() {
-  preLoad();
-  setTimeout(function() {
-    opacityAn('.loading', '0');
+if ($(window).width() > 700) {
+  $(window).load(function() {
+    preLoad();
     setTimeout(function() {
-      $('.loading').css('z-index', '0');
+      opacityAn('.loading', '0');
       setTimeout(function() {
-        opacityAn('.menu', '1');
-      }, 600);
-    }, 300);
-  }, 800);
-});
+        $('.loading').css('z-index', '0');
+        setTimeout(function() {
+          opacityAn('.diagonal', '.7');
+          opacityAn('.menu', '1');
+          setTimeout(function() {
+            opacityAn('.enter-in', '1');
+          }, 800);
+        }, 400);
+      }, 100);
+    }, 500);
+  });
+} else {
+  $(window).load(function() {
+    preLoad();
+    setTimeout(function() {
+      opacityAn('.diagonal', '.7');
+      opacityAn('.menu', '1');
+      setTimeout(function() {
+        opacityAn('.enter-in', '1');
+      }, 800);
+    }, 400);
+  });
+}
 
 $(document).ready(function() {
 
@@ -349,15 +433,30 @@ $(document).ready(function() {
   var moreRando;
   var emoreRando;
 
-  function randoRotate() {
-    rando = Math.random() * 360;
-    rando = Math.floor(rando);
+  function randoPlace() {
     moreRando = Math.random() * 40;
     moreRando = Math.floor(moreRando);
     emoreRando = Math.random() * 80;
     emoreRando = Math.floor(emoreRando);
+    $('.diagonal').css({'top': emoreRando + '%', 'left': moreRando + '%'});
 
-    $('.diagonal').css({'transform': 'rotate(' + rando + 'deg)', 'top': emoreRando + '%', 'left': moreRando + '%'});
+  }
+
+  function randoSpin() {
+    rando = Math.random() * 360;
+    rando = Math.floor(rando);
+    var circ = rando - 1;
+    $('.diagonal').css('-webkit-transform', 'rotate(' + rando + 'deg)');
+    $('.diagonal').css('-ms-transform', 'rotate(' + rando + 'deg)');
+    $('.diagonal').css('transform', 'rotate(' + rando + 'deg)');
+    setTimeout(function() {
+      randoSpin();
+    }, 60000);
+  }
+
+  function randoRotate() {
+    randoPlace();
+    randoSpin();
   }
 
   randoRotate();
@@ -373,9 +472,10 @@ $(document).ready(function() {
     flash('a.black-sand-white-sand-grey-sand', '#555', strobe7);
     flash('a.a-little-bit-cooler', '#555', strobe8);
     flash('a.screenshots', '#555', strobe9);
+    flash('a.net-presence', '#555', strobe10);
   }
-  
-  
+
+
   /// open up the menu/close the menu
 
   $('.menu-open').click(function(event) {
@@ -403,7 +503,7 @@ $(document).ready(function() {
   $(window).on('scroll', function() {
     var scrollTop = $(this).scrollTop() + 200;
 
-    $('.pseudorandom img, .rising-falling img, .rising-falling iframe, .screenshots img, .suggestions img, .black-sand-white-sand-grey-sand img, .a-little-bit-cooler img').each(function() {
+    $('.pseudorandom img, .rising-falling img, .rising-falling iframe, .screenshots img, .net-presence img, .suggestions img, .black-sand-white-sand-grey-sand img, .a-little-bit-cooler img').each(function() {
       var topDistance = $(this).offset().top;
       var pos = topDistance - scrollTop;
       var num = $(this).data('order');
@@ -424,12 +524,8 @@ $(document).ready(function() {
       $('.menu').css({'position':'relative', 'height':'auto'});
       $('.menu-dummy').css('position', 'relative');
     }
-    
+
     $('.menu-open').removeClass('open');
-    // footer();
-    // setTimeout(function() {
-    //   $('.menu-dummy').css('visibility', 'hidden');
-    // }, 2000);
     $('.view').css('z-index', '-1');
     history.pushState('', 'Mitchell Barton', "/");
   });
@@ -459,6 +555,20 @@ $(document).ready(function() {
     var path = location.pathname;
     var page = path.substring(location.pathname.lastIndexOf('/') + 1);
 
+    if ($('.fullscreen').hasClass('zoomed')) {
+      opacityAn('.fullscreen img', '0');
+      $('.fullscreen').removeClass('zoomed');
+      setTimeout(function() {
+        $('.fullscreen').css({'visibility':'hidden', 'z-index':'-10'});
+        $('.fullscreen img').remove();
+        $('.menu-open, .view').css('visibility', 'visible');
+        $('body').css('overflow-y', 'scroll');
+        setTimeout(function() {
+          opacityAn('.view', '1');
+        }, 300);
+      }, 300);
+    }
+
     if (path === '' || path === '/') {
       clearOut();
       menuFadeIn();
@@ -466,7 +576,7 @@ $(document).ready(function() {
         $('.menu').css({'position':'relative', 'height':'auto'});
         $('.menu-dummy').css('position', 'relative');
       }
-      
+
       $('.view').css('z-index', '-1');
     } else {
       switchPage();
